32c7228c816fbdacf57ed70270d3af2425a80e2b
anastasiajsokol <anastasia.j.sokol@gmail.com>
2023-04-23 21:37:17 -0500

    title screen

--- presentation/intro.py
+++ presentation/intro.py
@@ -1,2 +1,17 @@
-class Intro(Scene):
-  """Introduction scene"""
+from manim import *
+from manim_slides import Slide
+
+class Intro(Slide):
+  def construct(self):
+    title = Tex("Fourier Transforms")
+    subtitle = Tex("And drawing pictures with rotating circles")
+
+    title.scale(2)
+    subtitle.scale(0.8)
+    
+    subtitle.next_to(title, direction = DOWN)
+
+    self.play(Write(title))
+    self.play(Write(subtitle))
+
+    self.next_slide()
