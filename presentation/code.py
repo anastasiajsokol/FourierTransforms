4a3d4e702212f51a38a9409be5252f1a53cdcc52
Anastasia Sokol <anastasia.j.sokol@gmail.com>
2023-04-26 10:45:00 -0500

    update code to refactored version

--- presentation/code.py
+++ presentation/code.py
@@ -12,13 +12,13 @@ class Code(Slide):
         y_coefs = np.zeros(number_of_coefficents, dtype=(float, 2))
 
         for n in range(number_of_coefficents):
-            sum_cos_x = sum((2 / J * x_data[j] * cos(n * 2 * pi * j / J) for j in range(J)))
-            sum_cos_y = sum((2 / J * y_data[j] * cos(n * 2 * pi * j / J) for j in range(J)))
-            sum_sin_x = sum((2 / J * x_data[j] * sin(n * 2 * pi * j / J) for j in range(J)))
-            sum_sin_y = sum((2 / J * y_data[j] * sin(n * 2 * pi * j / J) for j in range(J)))
+            sum_cos_x = sum((2 / J * x_data[j] * cos(n * tau * j / J) for j in range(J)))
+            sum_cos_y = sum((2 / J * y_data[j] * cos(n * tau * j / J) for j in range(J)))
+            sum_sin_x = sum((2 / J * x_data[j] * sin(n * tau * j / J) for j in range(J)))
+            sum_sin_y = sum((2 / J * y_data[j] * sin(n * tau * j / J) for j in range(J)))
 
-            x_coefs[n] = (sqrt(sum_cos_x ** 2 + sum_sin_x ** 2), atan2(sum_sin_x, sum_cos_x)) # cosx, sinx, radius, alpha
-            y_coefs[n] = (sqrt(sum_cos_y ** 2 + sum_sin_y ** 2), atan2(sum_sin_y, sum_cos_y)) # cosy, siny, radius, alpha
+            x_coefs[n] = (sqrt(sum_cos_x ** 2 + sum_sin_x ** 2), atan2(sum_sin_x, sum_cos_x)) # radius, alpha
+            y_coefs[n] = (sqrt(sum_cos_y ** 2 + sum_sin_y ** 2), atan2(sum_sin_y, sum_cos_y)) # radius, alpha
 
             if n == 0:
                 self.origin = (sum_cos_x, sum_cos_y)
